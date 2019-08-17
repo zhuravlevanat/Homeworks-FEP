d733dbf76b6e881b5f7d2aabffdbd9a52cf3186f
Natalia Zhuravlova <zhyravlevanat@gmail.com>
2019-08-17 23:55:32 +0300

    add evenNumbers, numberGuesser, copyObject

--- lesson6/script.js
+++ lesson6/script.js
@@ -1,40 +1,36 @@
 'use strict';
 
-// function copyObject(obj) {
-//   const copyOfObj = {}; 
+function copy(obj) {
+  const copyOfObj = {}; 
 
-//   for (let prop in obj) {
-//     if (typeof obj[prop] !== 'object') {
-//       copyOfObj[prop] = obj[prop];
-//     } else {
-//       copyOfObj[prop] = copyObject(obj[prop]);
-//     }
-//   }
-//   return copyOfObj;   
-// }
+  for (let prop in obj) {
+    if (typeof obj[prop] !== 'object') {
+      copyOfObj[prop] = obj[prop];
+    } else {
+      copyOfObj[prop] = copy(obj[prop]);
+    }
+  }
+  return copyOfObj;   
+}
 
-// const person = {
-//   name: 'Matt',
-//   age: 25,
-//   address: {
-//     city: 'Dnipro',
-//     street: 'Kabardinska',
-//   }
-// };
+const person = {
+  name: 'Matt',
+  age: 25,
+  address: {
+    city: 'Dnipro',
+    street: 'Kabardinska',
+  }
+};
 
-// const teacher = copyObject(person);
+const teacher = copy(person);
 
-// teacher.address.city = 'Kyiv';
+teacher.address.city = 'Kyiv';
 
-// console.log(person, teacher);
+console.log(person, teacher);
 
-function cazino() {
-  let points = 0;
-  let userNumber;
-  let randomNumber;
-  let guessedNumber;
 
-  
+
+function numberGuesser() {
   alert 
   (`  I have number between 0 and 10. You have to guess my number. 
   I'll help you and I'll tell you if it's too high or too low, or correct. 
@@ -42,36 +38,58 @@ function cazino() {
   In the end I'll show you all your scores. 
   Good luck!!! Let\`s start`);
 
- const checkNumber= function () {
-  randomNumber = (min, max) => Math.round(min + Math.random()*(max - min));
-  guessedNumber = randomNumber(0, 10);
-  console.log(guessedNumber);
+ const checkNumber = function () {
+  let points = 0;
+  const randomNumber = (min, max) => Math.round(min + Math.random()*(max - min));
+  const guessedNumber = randomNumber(0, 10);
+  
   do {    
-    userNumber = +prompt('What is your guess?', '');
+    let userNumber = prompt('What is your guess?', '');
+    if (userNumber === null) break;
+    userNumber = userNumber.trim();    
+    if (userNumber < 0 || userNumber > 10 || userNumber ==="" || isNaN(userNumber)) {
+      alert('Enter number between 0 and 10'); 
+      continue;
+    }    
+    userNumber = Number(userNumber);
     if (userNumber === guessedNumber) {
-        points+=10;  
-        alert ('You are rigth! The guessed number was '+ guessedNumber);    
-        break;  
-      } else if (userNumber > guessedNumber) {
-          alert('It\'s too high. Try once more!'); continue;
-      } else if(userNumber < guessedNumber) {
-          alert('It\'s too low. Try once more!'); continue;
-        } 
-    } while (userNumber);
+      points+=10;  
+      alert ('You are right! The guessed number was '+ guessedNumber);    
+      break;  
+    } else if (userNumber > guessedNumber) {
+      alert('It\'s too high. Try once more!'); continue;
+    } else {
+      alert('It\'s too low. Try once more!'); continue;
+    } 
+  } while (true);
 
-    const confirmationTocontinue = confirm('Do you want to continue');
-    if (confirmationTocontinue) {
-      checkNumber();
-    } else alert('Total score: ' + points);
-  } 
- 
-
+  const confirmationToContinue = confirm('Do you want to continue');
+  if (confirmationToContinue) {
+    checkNumber();
+  } else {
+    return alert('Total score: ' + points);
+  }
+} 
 checkNumber();
 }
 
+numberGuesser();
 
+function getNumberOfEvenNumbers() {
+  let userNumber;
+  let numbers;
+  let numberOfEvenNumbers = 0;  
+  do {
+    userNumber = prompt('Please, enter your number', '');
+  } while (isNaN(userNumber) || userNumber ==="" || userNumber===null);
 
+  numbers = userNumber.split('');
+  numbers.forEach(function(value) {
+    if (value % 2 === 0) {
+      numberOfEvenNumbers += 1;
+    }
+  })
+  return alert('Number of even numbers is ' + numberOfEvenNumbers);  
+}
 
- 
-
-cazino();
+getNumberOfEvenNumbers();
