53422fe650d2ffe780065beddd1e2bdbf9d18ce8
Natalia Zhuravlova <zhuravlevanat@gmail.com>
2019-08-29 14:33:44 +0300

    add functions in greeting and min, max numbers

--- lesson 7/script.js
+++ lesson 7/script.js
@@ -1,55 +1,55 @@
 'use strict';
 
-let name;
-let numbers;
-
 const greeting = document.querySelector('#greeting');
 const maxNumber = document.querySelector('#max');
 const minNumber = document.querySelector('#min');
 
+function userInput(text) {
+  let input;
 
-do {
-  name = prompt('What is your name', '');
-  }  
-while (!name); 
+  do {
+    input = prompt(text, "");
+  } while(!input);
 
-greeting.innerHTML = 'Hello, ' + name;
-
-do {
-  numbers = prompt('Please, enter numbers separated by commas', '');
-  }  
-while (!numbers); 
-
-validateNumbers(numbers);
-
-
-function validateNumbers (numbers) {
-  const arr = numbers.split(',');
-  const notNumber = arr.some(value => isNaN(value));
-
-  if (notNumber) {
-    maxNumber.style.width = "200px";
-    maxNumber.style.height = "20px";
-    maxNumber.style.backgroundColor = "red";
-    maxNumber.innerHTML = 'Please, enter correct numbers';
-    
-    minNumber.style.backgroundColor = "red"; 
-  } else {
-    arr.sort(function(a, b) {
-      return a-b;
-    });
-
-    maxNumber.innerHTML = "Maximum number is " + arr[arr.length-1];
-    minNumber.innerHTML = "Minimum number is " + arr[0];
-  }
-
-
-
-
-  console.log(notNumber);
- 
-
+  return input;
 }
+
+const name = userInput('What is your name');
+const numbers = userInput('Please, enter numbers separated by commas');
+
+const arr = numbers.split(',');
+
+displayMessage(name, arr);
+
+function displayMessage(name, arr) { 
+  greeting.innerHTML = "Hello, " + name;
+  if (checkNaN(arr)) {
+      displayError();
+    } else {
+      displayMinAndMax(arr);
+}
+
+function checkNaN(arr) {
+  return arr.some(value => isNaN(value))? true: false;
+}
+
+function displayError() {
+  maxNumber.style.width = "200px";
+  maxNumber.style.height = "20px";
+  maxNumber.style.backgroundColor = "red";
+  maxNumber.innerHTML = 'Please, enter correct numbers';  
+}
+
+function displayMinAndMax(arr) {
+  arr.sort(function(a, b) {
+    return a-b;
+  });
+
+  maxNumber.innerHTML = "Maximum number is " + arr[arr.length-1];
+  minNumber.innerHTML = "Minimum number is " + arr[0];
+}
+}
+
 
 
 
