a632e0a574e4abcbfc942130002c6ab4ccab2e14
Natalia Zhuravlova <zhuravlevanat@gmail.com>
2019-09-10 14:54:31 +0300

    function constructor, averageMark

---
+++ lesson 11/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function Student(name, arrMarks) {
+  this.name = name;
+  this.arrMarks = arrMarks;
+
+  this.getAverageMark = function(){    
+    const totalMarks = this.arrMarks.reduce((total, value) => total + value);
+    return +(totalMarks/this.arrMarks.length).toFixed(1);
+ }     
+}
+
+function getAverageGroupMark(arrStudents) {
+  const totalGroupMark = arrStudents.reduce(
+                (total, value) => total + value.getAverageMark(), 0);
+  return +(totalGroupMark/arrStudents.length).toFixed(1);
+}
+
+const students = [ 
+  new Student('Student 1', [10,9,8,0,10]), // имя, оценки
+  new Student('Student 12', [10,0,8,0,3,4])
+ ];
+
+ console.log(students);
+
+ console.log(students[0].getAverageMark());
+ console.log(students[1].getAverageMark());
+
+ console.log(getAverageGroupMark(students));
