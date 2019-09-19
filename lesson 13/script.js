f5403eca3b15e2bba7a6a4b443b39282847ee26e
Natalia Zhuravlova <zhuravlevanat@gmail.com>
2019-09-19 15:15:23 +0300

    gallery of photos

--- lesson 13/script.js
+++ lesson 13/script.js
@@ -1,32 +1,21 @@
-'use strict';
+'use strict';  
+let intervalId; 
 
-  
-// class Gallery{
-
-// }
-
-// const myGallery = new Gallery(
-//                         document.getElementById('container')
-//                         )
-
-/* Опциональное задание - реализовать такие методы */
-
-// myGallery.show(2);
-// myGallery.next();
-// myGallery.prev();
 class Gallery {
   constructor(elem) {
     this.elem = elem;
-    this.index = 0;
+    this.index = 0;       
   }
 
   createGallery() {    
     const gallery = document.createElement('div');
     gallery.classList='gallery';
-    gallery.innerHTML = `<button class="gallery-button gallery-left-button">&lt;</button>
-                         <button class="gallery-button gallery-close-button">&#10006;</button>
-                         <div class="gallery-item"></div>
-                         <button class="gallery-button gallery-right-button">&gt;</button>`;
+    gallery.innerHTML = `
+      <button class="gallery-button gallery-left-button">&lt;</button>
+      <button class="gallery-button gallery-stop-button">&#9724;</button>
+      <div class="gallery-item"></div>
+      <button class="gallery-button gallery-right-button">&gt;</button>
+      `;
     document.querySelector('body').prepend(gallery);
   }
 
@@ -38,7 +27,7 @@ class Gallery {
     document.querySelector('.gallery-item').innerHTML = image.innerHTML;    
   }
 
-  showGalleryItem () {
+  showGalleryItems () {
     const galleryItems = this.getGalleryItems();
     this.setPhotoInGallery(galleryItems[this.index]);
   }
@@ -53,25 +42,36 @@ class Gallery {
   prev() {
     const arr = this.getGalleryItems();
     this.index--;
-    if(this.index == arr.length) this.index = 0;
+    if(this.index < 0) this.index = arr.length-1;
     this.setPhotoInGallery(arr[this.index]);
   }
-  init () { 
-    this.showGalleryItem ();
-    setInterval(this.next.bind(this), 3000); 
+  initGallery () {
+    this.createGallery(); 
+    this.showGalleryItems();
+    intervalId = setInterval(this.next.bind(this), 3000);
+    this.listeners();    
   }
-  
 
-    listeners () {
-    const fn = this.next.bind(this);
-    document.querySelector('.gallery-left-button').addEventListener('click', fn);
-    document.querySelector('.gallery-left-button').addEventListener('click', this.prev.bind(this));
-    }
+  pauseGallery () {
+    setInterval(function(){
+      clearInterval(intervalId);
+    });
+ }  
+
+  listeners () {
+    this.next = this.next.bind(this);
+    this.prev = this.prev.bind(this);
+    this.pauseGallery = this.pauseGallery.bind(this);
+    document.querySelector('.gallery-left-button').addEventListener('click', this.next);
+    document.querySelector('.gallery-right-button').addEventListener('click', this.prev);
+    document.querySelector('.gallery-stop-button').addEventListener('click', this.pauseGallery);
+  }
 }  
 
 const myGallery = new Gallery(document.getElementById('container'));
-myGallery.createGallery();
-myGallery. showGalleryItem ();
-//myGallery.next();
-myGallery.listeners();
-//myGallery.init();
+
+myGallery.initGallery();
+
+// myGallery.show(2);
+// myGallery.next();
+// myGallery.prev();
