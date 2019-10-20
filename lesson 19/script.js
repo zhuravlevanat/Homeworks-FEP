6104d30bb761fa4f922da33484c3dd6368f4155c
Natalia Zhuravlova <zhuravlevanat@gmail.com>
2019-10-20 22:49:09 +0300

    photo gallery v3

--- lesson 19/script.js
+++ lesson 19/script.js
@@ -1,18 +1,21 @@
 'use strict';
 const PHOTO_SOURCE_URL = 'https://jsonplaceholder.typicode.com/photos';
-const ADD = 49;
 const PAGINATION_LINK_CLASS = 'page-link';
 const FIRST_ELEM_IN_PAG = 'first-page';
 const LAST_ELEM_IN_PAG = 'last-page';
-const MAX_PAGE_NUMBER = 100;
-const MIN_PAGE_NUMBER = 1;
+const GALLERY_ITEM_CLASS = 'photo-item';
 const HIDDEN_CLASS = 'hidden';
 const ACTIVE_CLASS = 'active';
 const DELETE_BTN_CLASS = 'delete-btn';
-const shiftedValue = 5;
-const GALLERY_ITEM_CLASS = 'photo-item';
-const GALLERY_ITEM_TEMPLATE = document.getElementById('galleryItemTemplate').innerHTML;
-const FULL_PHOTO_TEMPLATE = document.getElementById('fullPhotoTemplate').innerHTML;
+const SHIFTED_VALUE = 5;
+const NUMBER_OF_PHOTOS = 49;
+const MAX_PAGE_NUMBER = 100;
+const MIN_PAGE_NUMBER = 1;
+
+const GALLERY_ITEM_TEMPLATE = 
+      document.getElementById('galleryItemTemplate').innerHTML;
+const FULL_PHOTO_TEMPLATE = 
+      document.getElementById('fullPhotoTemplate').innerHTML;
 
 const galleryElement = document.getElementById('gallery');
 const fullPhoto = document.getElementById('full-photo');
@@ -20,23 +23,38 @@ const background = document.getElementById('background-layout');
 const pagination = document.getElementById('pagination');
 const prevEl = document.querySelector('.prev');
 const nextEL = document.querySelector('.next');
+let Arr;
+let currentPage;
 
-pagination.addEventListener('click', onPaginationClick);
+init();
+
+//Event Listeners
+
 galleryElement.addEventListener('click', onGalleryClick);
 fullPhoto.addEventListener('click', onFullPhotoClick);
+pagination.addEventListener('click', onPaginationClick);
 
-let Arr;
-let currentPage = 1;
 
 function onGalleryClick() {
   if (event.target.classList.contains(GALLERY_ITEM_CLASS))
       displayFullPhoto(event.target);
   }
 
-
 function onFullPhotoClick() {
   if (event.target.classList.contains(DELETE_BTN_CLASS))
   hideFullPhoto();
+}
+
+function displayFullPhoto(elem) {
+  displayBackground();
+  displayPhoto(elem);  
+}
+
+function displayPhoto(elem) {
+  const fullPhotoTemplate = 
+      FULL_PHOTO_TEMPLATE.replace('{{url}}', elem.dataset.url);                                                      
+                                            
+    fullPhoto.innerHTML = fullPhotoTemplate;
 }
 
 function hideFullPhoto() {
@@ -48,26 +66,17 @@ function hideBackground() {
   background.classList.add(HIDDEN_CLASS);
 }
 
-function displayFullPhoto(elem) {
-  displayBackground();
-  displayPhoto(elem);  
-}
-
 function displayBackground() {
   background.classList.remove(HIDDEN_CLASS);
 }
 
-function displayPhoto(elem) {
-  console.log(elem.dataset.url)
-  let fullPhotoTemplate = 
-      FULL_PHOTO_TEMPLATE.replace('{{url}}', elem.dataset.url);                                                      
-                                            
-    fullPhoto.innerHTML = fullPhotoTemplate;
-}
+// Pagination
 
 function onPaginationClick(event) {
   event.preventDefault();
+
   if (event.target.classList.contains(PAGINATION_LINK_CLASS)) {
+
     switch(true) {
       case event.target.classList.contains('next'):
         getNextPage();
@@ -77,104 +86,98 @@ function onPaginationClick(event) {
         break
       default: getCurrentPage(event.target);
     }
-    renderPhotosInGallery();
+
+    renderPhotosInGallery(currentPage);
+    setItemsInLS('currentPage', currentPage);
   }
  }
 
- function getPageElement() {
-  const coll = pagination.children;
-  for (let index = 1; index < coll.length-1; index++) {
-    if (coll[index].innerHTML == currentPage) {
-      return coll[index];
-    }     
-  } 
- }
-
-function getNextPage() {
+ function getNextPage() {
   const elem = getPageElement();
   if (elem.classList.contains(LAST_ELEM_IN_PAG)){
     shiftPagesToRight();
-    setCurrentPageActive(currentPage);
   } else {
-    currentPage++;
-    setCurrentPageActive(currentPage);
-  }
-    
+    currentPage++;    
+  } 
+  setCurrentPageActive(currentPage);   
 }
-
 
 function getPreviousPage() {
   const elem = getPageElement();
   if (elem.classList.contains(FIRST_ELEM_IN_PAG)){
-    shiftPagesToLeft();
-    
+    shiftPagesToLeft();    
   } else {
-    currentPage--;
-    
+    currentPage--;    
   }
+  setCurrentPageActive(currentPage);    
+}
 
+function getCurrentPage(elem) {
+  currentPage = elem.innerHTML;
   setCurrentPageActive(currentPage);
-    
+}
+
+function getPageElement() {
+  const pagItems = getPaginationItems();
+  for (let i = 1; i < pagItems.length-1; i++) {
+    if (pagItems[i].innerHTML == currentPage) {
+      return pagItems[i];
+    }     
+  } 
+ }
+
+function getPaginationItems() {
+  return pagination.children
 }
   
-function shiftPagesToLeft() { 
-  const coll = pagination.children;
-  for (let index = 1; index < coll.length-1; index++) {
-    const value = +coll[index].innerHTML;
+function shiftPagesToLeft() {  
+  const pagItems = getPaginationItems();
+  for (let i = 1; i < pagItems.length-1; i++) {
+    const value = +pagItems[i].innerHTML;
     if (value <= MIN_PAGE_NUMBER) {
       prevEl.classList.add(HIDDEN_CLASS);
       return;
     }    
-    coll[index].innerHTML= (value-shiftedValue);    
-  } 
-
-  currentPage = +coll[coll.length-2].innerHTML;
-    
+    pagItems[i].innerHTML= (value-SHIFTED_VALUE);    
+  }
+  currentPage = +pagItems[pagItems-2].innerHTML;    
 }
 
 function shiftPagesToRight() { 
-  const coll = pagination.children;
-  for (let index = 1; index < coll.length-1; index++) {
-    const value = +coll[index].innerHTML;
+  const pagItems = getPaginationItems();
+  for (let i = 1; i < pagItems.length-1; i++) {
+    const value = +pagItems[i].innerHTML;
     if (value >= MAX_PAGE_NUMBER) {
       nextEL.classList.add(HIDDEN_CLASS);
       return;
     }
-    coll[index].innerHTML= (value+shiftedValue);
-      
+    pagItems[i].innerHTML= value + SHIFTED_VALUE;      
   } 
-  currentPage = +coll[1].innerHTML;
-    
+  currentPage = +pagItems[1].innerHTML;    
 }
 
 function setCurrentPageActive(pageValue) {
-  console.log(currentPage);
   if (currentPage > 1 && currentPage < 100) {
     prevEl.classList.remove(HIDDEN_CLASS);
   }else {
     prevEl.classList.add(HIDDEN_CLASS);
   }
-  const coll = pagination.children;
-  for (let index = 1; index < coll.length-1; index++) {
-    coll[index].classList.remove(ACTIVE_CLASS);
-    if (coll[index].innerHTML == pageValue) {
-      coll[index].classList.add(ACTIVE_CLASS);
+  const pagItems = getPaginationItems();
+  for (let i = 1; i < pagItems.length-1; i++) {
+    pagItems[i].classList.remove(ACTIVE_CLASS);
+    if (pagItems[i].innerHTML == pageValue) {
+      pagItems[i].classList.add(ACTIVE_CLASS);
     }    
   }   
 }
 
-function getCurrentPage(elem) {
-  currentPage = elem.innerHTML;
-  console.log(currentPage);
-  setCurrentPageActive(currentPage);
+//Initialization
 
-}
-
-init();
-
-function init() {  
+function init() {
+  currentPage = JSON.parse(getItemsFromLS('currentPage')) || 1;
   getPhotos();
-  renderPhotosInGallery();
+  renderPhotosInGallery(currentPage);
+  setCurrentPageActive(currentPage);
 }
 
 function requestJson(url) {
@@ -184,26 +187,26 @@ function requestJson(url) {
 
 function getPhotos() {
   return requestJson(PHOTO_SOURCE_URL)
-  .then(setPhotoDataInLS);
+  .then((data) => setItemsInLS('photos', data));
 }
 
-function setPhotoDataInLS(data) {
-  return localStorage.setItem('photos', JSON.stringify(data));
+function setItemsInLS(name, data) {
+  return localStorage.setItem(name, JSON.stringify(data));
 }
 
-function getPhotosData() {
-  return localStorage.getItem('photos');
+function getItemsFromLS(name) {
+  return localStorage.getItem(name);
 }
-
 
 function photosDataForPage(page) {
-  const brr = JSON.parse(getPhotosData());
-  let photosForPage = brr.filter(data => data.id>(ADD*page-ADD) && data.id<(ADD*page))
+  const arrayOfPhotos = JSON.parse(getItemsFromLS('photos'));
+  let photosForPage = arrayOfPhotos.filter(data => 
+    data.id>(NUMBER_OF_PHOTOS*page-NUMBER_OF_PHOTOS) && data.id<(NUMBER_OF_PHOTOS*page))
   return photosForPage
 }
 
-function renderPhotosInGallery() {
-  const data = photosDataForPage(currentPage);
+function renderPhotosInGallery(page) {
+  const data = photosDataForPage(page);
   galleryElement.innerHTML = data.map(photo => {
     return GALLERY_ITEM_TEMPLATE.replace('{{id}}', photo.id)
                                 .replace('{{thumbnail-url}}', photo.thumbnailUrl)
