48a867131938057decb901bab5820bb454258b5d
Natalia Zhuravlova <zhuravlevanat@gmail.com>
2019-10-20 16:01:16 +0300

    add functionality

--- lesson 19/script.js
+++ lesson 19/script.js
@@ -1,78 +1,179 @@
 'use strict';
 const PHOTO_SOURCE_URL = 'https://jsonplaceholder.typicode.com/photos';
 const ADD = 49;
-const GALLERY_ITEM_TEMPLATE = document.getElementById('galleryItemTemplate').innerHTML;
 const PAGINATION_LINK_CLASS = 'page-link';
 const FIRST_ELEM_IN_PAG = 'first-page';
 const LAST_ELEM_IN_PAG = 'last-page';
-const ADDING = 5;
+const MAX_PAGE_NUMBER = 100;
+const MIN_PAGE_NUMBER = 1;
+const HIDDEN_CLASS = 'hidden';
+const ACTIVE_CLASS = 'active';
+const DELETE_BTN_CLASS = 'delete-btn';
+const shiftedValue = 5;
+const GALLERY_ITEM_CLASS = 'photo-item';
+const GALLERY_ITEM_TEMPLATE = document.getElementById('galleryItemTemplate').innerHTML;
+const FULL_PHOTO_TEMPLATE = document.getElementById('fullPhotoTemplate').innerHTML;
 
 const galleryElement = document.getElementById('gallery');
+const fullPhoto = document.getElementById('full-photo');
+const background = document.getElementById('background-layout');
 const pagination = document.getElementById('pagination');
+const prevEl = document.querySelector('.prev');
+const nextEL = document.querySelector('.next');
 
 pagination.addEventListener('click', onPaginationClick);
+galleryElement.addEventListener('click', onGalleryClick);
+fullPhoto.addEventListener('click', onFullPhotoClick);
 
 let Arr;
 let currentPage = 1;
+
+function onGalleryClick() {
+  if (event.target.classList.contains(GALLERY_ITEM_CLASS))
+      displayFullPhoto(event.target);
+  }
+
+
+function onFullPhotoClick() {
+  if (event.target.classList.contains(DELETE_BTN_CLASS))
+  hideFullPhoto();
+}
+
+function hideFullPhoto() {
+  fullPhoto.innerHTML = '';
+  hideBackground();
+}
+
+function hideBackground() {
+  background.classList.add(HIDDEN_CLASS);
+}
+
+function displayFullPhoto(elem) {
+  displayBackground();
+  displayPhoto(elem);  
+}
+
+function displayBackground() {
+  background.classList.remove(HIDDEN_CLASS);
+}
+
+function displayPhoto(elem) {
+  console.log(elem.dataset.url)
+  let fullPhotoTemplate = 
+      FULL_PHOTO_TEMPLATE.replace('{{url}}', elem.dataset.url);                                                      
+                                            
+    fullPhoto.innerHTML = fullPhotoTemplate;
+}
 
 function onPaginationClick(event) {
   event.preventDefault();
   if (event.target.classList.contains(PAGINATION_LINK_CLASS)) {
     switch(true) {
       case event.target.classList.contains('next'):
-        getNextPage(event.target);
+        getNextPage();
         break
       case event.target.classList.contains('prev'):
-        getPreviousPage(event.target);
+        getPreviousPage();
         break
-      default: getCurrentPage();
+      default: getCurrentPage(event.target);
     }
+    renderPhotosInGallery();
   }
  }
 
+ function getPageElement() {
+  const coll = pagination.children;
+  for (let index = 1; index < coll.length-1; index++) {
+    if (coll[index].innerHTML == currentPage) {
+      return coll[index];
+    }     
+  } 
+ }
+
 function getNextPage() {
+  const elem = getPageElement();
   if (elem.classList.contains(LAST_ELEM_IN_PAG)){
     shiftPagesToRight();
+    setCurrentPageActive(currentPage);
   } else {
     currentPage++;
+    setCurrentPageActive(currentPage);
   }
-    displayPhotos();  
+    
 }
 
-function getPreviousPage(elem) {
+
+function getPreviousPage() {
+  const elem = getPageElement();
   if (elem.classList.contains(FIRST_ELEM_IN_PAG)){
     shiftPagesToLeft();
+    
   } else {
     currentPage--;
+    
   }
-    displayPhotos();
+
+  setCurrentPageActive(currentPage);
+    
 }
   
 function shiftPagesToLeft() { 
   const coll = pagination.children;
   for (let index = 1; index < coll.length-1; index++) {
-    value = coll[index].innerHTML;
-    coll[index].innerHTML= (value-ADDING);    
-  }   
+    const value = +coll[index].innerHTML;
+    if (value <= MIN_PAGE_NUMBER) {
+      prevEl.classList.add(HIDDEN_CLASS);
+      return;
+    }    
+    coll[index].innerHTML= (value-shiftedValue);    
+  } 
+
+  currentPage = +coll[coll.length-2].innerHTML;
+    
 }
 
 function shiftPagesToRight() { 
   const coll = pagination.children;
   for (let index = 1; index < coll.length-1; index++) {
-    value = coll[index].innerHTML;
-    coll[index].innerHTML= (value+ADDING);    
+    const value = +coll[index].innerHTML;
+    if (value >= MAX_PAGE_NUMBER) {
+      nextEL.classList.add(HIDDEN_CLASS);
+      return;
+    }
+    coll[index].innerHTML= (value+shiftedValue);
+      
+  } 
+  currentPage = +coll[1].innerHTML;
+    
+}
+
+function setCurrentPageActive(pageValue) {
+  console.log(currentPage);
+  if (currentPage > 1 && currentPage < 100) {
+    prevEl.classList.remove(HIDDEN_CLASS);
+  }else {
+    prevEl.classList.add(HIDDEN_CLASS);
+  }
+  const coll = pagination.children;
+  for (let index = 1; index < coll.length-1; index++) {
+    coll[index].classList.remove(ACTIVE_CLASS);
+    if (coll[index].innerHTML == pageValue) {
+      coll[index].classList.add(ACTIVE_CLASS);
+    }    
   }   
 }
 
-function getCurrentPage() {
-  console.log('current');
+function getCurrentPage(elem) {
+  currentPage = elem.innerHTML;
+  console.log(currentPage);
+  setCurrentPageActive(currentPage);
+
 }
 
 init();
 
 function init() {  
   getPhotos();
-  createPagination();
   renderPhotosInGallery();
 }
 
@@ -97,7 +198,7 @@ function getPhotosData() {
 
 function photosDataForPage(page) {
   const brr = JSON.parse(getPhotosData());
-  let photosForPage = brr.filter(data => data.id<(ADD+page))
+  let photosForPage = brr.filter(data => data.id>(ADD*page-ADD) && data.id<(ADD*page))
   return photosForPage
 }
 
@@ -105,80 +206,7 @@ function renderPhotosInGallery() {
   const data = photosDataForPage(currentPage);
   galleryElement.innerHTML = data.map(photo => {
     return GALLERY_ITEM_TEMPLATE.replace('{{id}}', photo.id)
-                             .replace('{{thumbnail-url}}', photo.thumbnailUrl)
+                                .replace('{{thumbnail-url}}', photo.thumbnailUrl)
+                                .replace('{{full-photo-url}}', photo.url)
   }).join('');
 }
-
-function createPagination(initialValue) {
-  
-  
-}
-
-
-
-// this.getPhotos().then(data => {
-//   data.forEach(element => {
-//     this.createGallery(element.url, element.thumbnailUrl);       
-//   });                 
-// });
-
-// let todoList = [];
-
-// function onNewTodoFormSubmit(e) {
-//   e.preventDefault();
-
-//   createTodo({
-//     title: newTodoTitleElement.value
-//   });
-
-// }
-
-// function onListClick(e) {
-//   const id = event.target.dataset.todoId; 
-
-//   if(id) {
-//     deleteTodo(id);
-//   }
-// }
-
-// function deleteTodo(id) {
-//   todoList = todoList.filter((el => {
-//     return el.id != id;
-//   }))
-
-//   setTodoList(todoList);
-//   renderTodosList(todoList);
-// }
-
-// function createTodo(todo) {
-//   todo.id = Data.now();
-//   todoList.push(todo);
-
-//   setTodoList(todoList);
-//   renderTodosList(todoList);
-// }
-
-// function setTodoList(data) {
-//   return localStorage.setItem('todo', JSON.stringify(data));
-// }
-
-// function init() {
-//   let todoList = getTodosList();
-//   todoList = todoList ? JSON.parse(todoList) : []
-
-//   renderTodosList(todoList);
-// }
-
-
-
-// function getTodosList() {
-//   return localStorage.getItem('todos');
-// }
-
-// function renderTodosList(data) {
-//   listElement.innerHTML = data.map(todo => {
-//     return TODO_ITEM_TEMPLATE.replace('{{id}}', todo.id)
-//                              .replace('{{title}}', todo.title)
-//   }).join('');
-// }
-
