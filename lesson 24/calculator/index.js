3af048befa12cbec2f3c258c18ac772d0504ed01
Natalia Zhuravlova <zhuravlevanat@gmail.com>
2019-11-05 11:35:00 +0200

    Calculator NodeJS

---
+++ lesson 24/calculator/index.js
@@ -0,0 +1,11 @@
+const add = require('./add');
+const mult = require('./mult');
+const div = require('./div');
+const sub= require('./sub');
+
+module.exports = {
+  add,
+  mult,
+  div,
+  sub
+}
---
+++ lesson 24/main.js
@@ -0,0 +1,6 @@
+const calculator = require('./calculator');
+
+console.log(calculator.add(2,3)) // 5
+console.log(calculator.sub(2,3)) // -1
+console.log(calculator.mult(2,3)) // 6
+console.log(calculator.div(6,3)) // 2
